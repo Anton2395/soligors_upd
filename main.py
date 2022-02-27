3d84bd193cd83c0cd46602704adf258140a8eadf
tohashilin <toha2395@gmail.com>
2022-02-27 21:47:13 +0300

    first request method

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from sqlalchemy.orm import sessionmaker
+
+from DataBase.models import engine
+from DataBase.models import TypeStation, Station
 
 app = FastAPI()
 
+Session = sessionmaker(bind=engine)
 
 @app.get("/")
 def index():
-    return "hihihihi"
+    ses = Session()
+    data = ses.query(Station).all()
+    return data
 
 
 
